36c027bfd2ea9f245a382b96e94309a6f6deb3e0
Anders <andershurum@github.com>
2017-06-17 17:11:10 +0200

    finished knockoutJS assignment

--- ud989-cat-clicker-ko-starter/js/app.js
+++ ud989-cat-clicker-ko-starter/js/app.js
@@ -1,7 +1,25 @@
-var ViewModel = function() {
+
+var initialdata = [
+  {
+        name:"Tabby2",
+        img: "img/cat1.jpg",
+        nicks: [{name:"Goldylocks"},
+                {name:"Upking"},
+                {name: "Teddytalks"}]
+  },
+  {
+        name:"Shooby",
+        img: "img/cat2.jpg",
+        nicks: [{name:"CaptainShoob"},
+                {name:"Candymeow"},
+                {name:"StarPurr"}]
+  }
+]
+
+var Cat = function(data){
   this.clickCount = ko.observable(0);
-  this.name = ko.observable("Tabby");
-  this.imgSrc = ko.observable("img/cat1.jpg");
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.img);
   this.level = ko.computed(function(){
     if (this.clickCount() > 30){
       return "MEGACAT!!!";
@@ -12,9 +30,30 @@ var ViewModel = function() {
     }
   }, this)
 
+  this.nicks = ko.observableArray(data.nicks)
+
+}
+
+
+var ViewModel = function() {
+  self = this;
+
+  this.catList = ko.observableArray([]);
+
+  initialdata.forEach(function(catItem){
+    self.catList.push(new Cat(catItem));
+  });
+
+  self.currentCat = ko.observable( this.catList()[0] );
+
   this.incrementCounter = function(){
     this.clickCount(this.clickCount()+1);
   };
+
+  self.setcurrentCat = function(){
+    console.log("changing cat..")
+    self.currentCat(this);
+  }
 }
 
 ko.applyBindings(new ViewModel());
